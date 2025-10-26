e3fb56311e3c55255f137de06381819d89b66689
Johnny Matheus <alunotioficial@gmail.com>
2025-10-26 19:04:44 -0300

    Create index.js

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,23 @@
+// Mais de um valor
+const nome = 'Johnny'
+const x = 10;
+const y = "Algum texto"
+const z = [1,2]
+
+console.log(x,y,z)
+
+//contagem de impressões
+
+console.count(`O valor de x é ${x}, cotagem`)
+console.count(`O valor de x é ${x}, cotagem`)
+console.count(`O valor de x é ${x}, cotagem`)
+console.count(`O valor de x é ${x}, cotagem`)
+
+//Variavel entre string
+console.log(`O nome é %s e ele é programador`,nome)
+
+
+// Limpar o console
+setTimeout(() => {
+    console.clear
+})
