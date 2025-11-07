01087dc24796b060d1ebceeaa481c4af9fdaa168
Johnny Matheus <alunotioficial@gmail.com>
2025-11-07 16:01:07 -0300

    MODULO URL

--- 3_CORE_MODULES/2_retornando_html/index.js
+++ 3_CORE_MODULES/2_retornando_html/index.js
@@ -5,7 +5,7 @@ const port = 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Contenty-type', 'text/html')
-    res.end('<h1>Olá, este é o meu prmeiro server html</h1>')
+    res.end('<h1>Olá, este é o meu prmeiro server html</h1><p>Testando atualização</p>')
 })
 
 server.listen(port, () => {
