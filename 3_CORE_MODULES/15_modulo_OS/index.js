ff0e050d9c40129c9614ec8a7878e5e3f1e4dfec
Johnny Matheus <alunotioficial@gmail.com>
2025-11-13 20:57:53 -0300

    Instalando pacotes

---
+++ 3_CORE_MODULES/15_modulo_OS/index.js
@@ -0,0 +1,9 @@
+const os = require('os')
+
+console.log(os.cpus())//quantos cpus tem no servidor
+
+console.log(os.freemem()) //quanto de memoria livre
+
+console.log(os.homedir())
+ 
+console.log(os.type())
---
+++ 4_FUNDAMENTOS_NPM/3_instalando_pacote/index.js
@@ -0,0 +1,13 @@
+const _ = require('lodash')
+console.clear()
+const a = [1,2,3,4,5]
+const b = [2,4,6,7,8]
+
+const diff = _.difference(a,b)
+
+console.log(diff)
+
+
+
+
+
