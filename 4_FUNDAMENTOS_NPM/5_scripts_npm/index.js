5381cb92c9710e561448ebb46c9defa3c2cec8cc
Johnny Matheus <alunotioficial@gmail.com>
2025-11-16 03:16:16 -0300

    estudando

---
+++ 4_FUNDAMENTOS_NPM/5_scripts_npm/index.js
@@ -0,0 +1,10 @@
+import _ from 'lodash'
+import chalk from 'chalk'
+console.clear()
+
+const a = [1,2,3,4,5]
+const b = [2,4,5,6,7,8]
+
+const diff = _.difference(a,b)
+
+console.log(`O que tem em A que não tem em B ${chalk.bgRed.bold(diff)}`)
---
+++ 4_FUNDAMENTOS_NPM/6_pacote_global/index.js
@@ -0,0 +1,5 @@
+const _ = require('lodash')
+
+const lista = [1,2,2,3,4,5,5,6,6,8]
+
+console.log(_.sortedUniq(lista))
